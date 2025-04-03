699ac4b0fb3c4a6bb4b644d7acd5f382baa6abc9
Timoffev Nicolai <nicolai.tm@gmail.com>
2025-04-04 01:31:33 +0300

    v1

---
+++ startSWT.bat
@@ -0,0 +1,6 @@
+rem E:\prgs\j\graalvm-jdk-24+36.1\bin\java -jar .\target\SequrFly-1.0-SNAPSHOT-jar-with-dependencies.jar
+E:\prgs\j\graalvm-jdk-24+36.1\bin\java -agentlib:native-image-agent=config-output-dir=native-config -jar .\target\SequrFly-1.0-SNAPSHOT-jar-with-dependencies.jar
+
+
+
+pause
